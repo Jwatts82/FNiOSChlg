95c270147855fa4ac3b15e62b7e91e6c0145ab34
jwatts82 <wattscodes@gmail.com>
2021-05-23 10:45:05 -0700

    create IBO

--- FNiOSChlg/ViewController.swift
+++ FNiOSChlg/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var tableview: UITableView!
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
